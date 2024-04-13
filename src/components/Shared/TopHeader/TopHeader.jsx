12fe1c2a3cce8a48b5fa7d0070cca63dddd4e969
vijaynaya <vijaynayak4808@gmail.com>
2024-04-14 02:57:59 +0300

    add shared components footer header

---
+++ src/components/Shared/TopHeader/TopHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaGithubSquare, FaPhoneAlt, FaEnvelope  } from "react-icons/fa";
+
+const TopHeader = () => {
+    return (
+        <div id="topbar" className="d-flex align-items-center fixed-top">
+
+        </div>
+    );
+};
+export default TopHeader;
