97350a68b4ba875e7c6d0ef3bbf286198740bd7b
vijaynaya <vijaynayak4808@gmail.com>
2024-04-14 03:03:34 +0300

    add home page components and pages sections

---
+++ src/components/Home/Gallery/Gallery.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import img2 from '../../../images/features/baby.png';
+import img3 from '../../../images/doc/doc4.jpg';
+import img4 from '../../../images/doc/doctor 5.jpg';
+import img5 from '../../../images/doc/doc1.jpg';
+import img6 from '../../../images/doc/doctor chair 2.jpg';
+import img7 from '../../../images/features/feature-03.jpg';
+import img8 from '../../../images/doc/doctor 5.jpg';
+import img9 from '../../../images/doc/doctor chair 2.jpg';
+import './index.css';
+import { Image } from 'antd';
+
+const Gallery = () => {
+    const imageArray = [img2,img3,img4,img5,img6,img7, img8, img9]
+    return (
+        <section className="gallery container">
+
+        </section>
+    )
+}
+
+export default Gallery
